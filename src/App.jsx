1325b11ebf73372d1c0b32059b171331d3150ded
daniele <daniele.pagliero@gmail.com>
2025-10-21 11:01:08 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main className="container">
+      <h1>Marklee</h1>
+      <p>Your app starts here.</p>
+    </main>
+  );
+}
+
+export default App;
